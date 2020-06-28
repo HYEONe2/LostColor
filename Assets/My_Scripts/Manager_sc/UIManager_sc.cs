480f278106c64a41aba8610aea5ea50b4a20da82
Dayoung Lee <rkek3766@gmail.com>
2020-06-28 23:33:41 +0900

    ui추가중

---
+++ Assets/My_Scripts/Manager_sc/UIManager_sc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_sc : MonoBehaviour
+{
+    public Slider BackVolume;
+    public AudioSource audio;
+
+    private float backVol = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backVol = BackVolume.value;
+        PlayerPrefs.SetFloat("backvol", backVol);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoundSlider();
+    }
+
+    public void SoundSlider()
+    {
+        audio.volume = BackVolume.value;
+
+        backVol = BackVolume.value;
+        PlayerPrefs.SetFloat("backvol", backVol);
+    }
+}
