fe9188f0cadd1a6f4790af87ee1b903496f8dfc2
Dayoung Lee <rkek3766@gmail.com>
2020-08-05 02:24:25 +0900

    스테이지3 땅 수정& 몬스터 네비메쉬 이동, 공격 추가중

---
+++ Assets/My_Scripts/Monster_sc/BombAttack_sc.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombAttack_sc : MonoBehaviour
+{
+    GameObject Target;
+    Vector3 TargetPos;
+
+    public bool m_bPlayerUse = false;
+    private GameObject Monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.Find("Player");
+        TargetPos = Target.transform.position;
+        Destroy(gameObject, 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fDist = (TargetPos - gameObject.transform.position).magnitude;
+
+        Vector3 vDir = (TargetPos - gameObject.transform.position).normalized;
+        if (fDist > 1)
+            gameObject.transform.position += vDir * Time.deltaTime * 11.0f;
+    }
+}
